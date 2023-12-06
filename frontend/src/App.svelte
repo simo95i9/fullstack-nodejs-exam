52be582e3044856958504c20409e57186c7490a5
Simon Gredal <simon.gredal@icloud.com>
2023-12-06 15:32:48 +0100

    Start frontend with svelte template

---
+++ frontend/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<p>hello world</p>
+
+<style>
+
+    main {
+        min-height: 100vh;
+        padding: 1em;
+    }
+
+</style>
---
+++ frontend/src/main.js
@@ -0,0 +1,7 @@
+import './reset.css'
+import './style.css'
+
+import App from './App.svelte'
+
+const app = new App({ target: document.getElementById('app') })
+export default app
