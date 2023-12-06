cdc088fc91e6795bf4fbc5175be9520ed6970ce7
Simon Gredal <simon.gredal@icloud.com>
2023-12-06 16:39:48 +0100

    Create mock routes with svelte-routing

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,8 +1,33 @@
 <script>
+    import { Router, Link, Route } from 'svelte-routing'
 
+    import Home from './routes/Home.svelte'
+    import About from './routes/About.svelte'
+    import Events from './routes/Events.svelte'
+    import Profile from './routes/Profile.svelte'
+    import SignIn from './routes/SignIn.svelte'
+    import SignUp from './routes/SignUp.svelte'
+    import SignOut from './routes/SignOut.svelte'
+
+    import NavigationMenu from './components/NavigationMenu.svelte'
+    import ProtectedRoute from './lib/ProtectedRoute.svelte'
+
+    export let url = document.location.pathname
 </script>
 
-<p>hello world</p>
+<Router {url}>
+    <NavigationMenu />
+
+    <main>
+        <Route path='/' component={ Home } />
+        <Route path='/about' component={ About } />
+        <Route path='/events' component={ Events } />
+        <Route path='/signin' component={ SignIn } />
+        <Route path='/signup' component={ SignUp } />
+        <ProtectedRoute path='/profile' component={ Profile } />
+        <ProtectedRoute path='/signout' component={ SignOut } />
+    </main>
+</Router>
 
 <style>
 
